93c89b8fa4ccbfdd7382b256da28a0adacca0d4f
the-madhur <madhurhp245@gmail.com>
2025-02-19 11:49:32 +0530

    Initial commit

---
+++ src/IntroTopics/HelloWorld.java
@@ -0,0 +1,8 @@
+package IntroTopics;
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/IntroTopics/Variables.java
@@ -0,0 +1,17 @@
+package IntroTopics;
+
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        int speed = 20;
+        float flour = 20.5f;
+        double digit = 20.25;
+
+        System.out.println(speed);
+        System.out.println(flour);
+        System.out.println(digit);
+
+
+    }
+}
