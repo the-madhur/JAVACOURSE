73305b1ed38d03e2aa5c9903af41e5d0ac0b9848
the-madhur <madhurhp245@gmail.com>
2025-03-01 15:48:44 +0530

    initial Commit

---
+++ src/Strings/ReverseString.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class ReverseString 
+{
+        public static void main(String[] args) {
+            
+            String str1 = "madhur";
+            String str2 = " ";
+    
+            for(int i = str1.length() - 1; i>=0; i--)
+            {
+                str2 = str2 + str1.charAt(i);
+            }
+    
+            System.out.println(str2);
+        }
+
+}
