ed21cc3492e3c41f9da084aa847f5f7d8e3f68df
the-madhur <madhurhp245@gmail.com>
2025-03-04 11:06:45 +0530

    Abstract commit

---
+++ src/Bank/BankAccount.java
@@ -0,0 +1,17 @@
+package Bank;
+
+public abstract class BankAccount 
+{
+    public void accNo()
+    {
+
+    }
+
+    public void accBalance()
+    {
+
+    }
+
+    public abstract void deposit();
+    public abstract void withdraw();
+}
