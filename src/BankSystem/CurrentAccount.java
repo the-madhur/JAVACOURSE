09969744f692072d7aba5b058b41dc7116ad7efd
the-madhur <madhurhp245@gmail.com>
2025-03-04 14:38:39 +0530

    CA commit

---
+++ src/BankSystem/CurrentAccount.java
@@ -0,0 +1,39 @@
+package BankSystem;
+
+public class CurrentAccount extends BankAccount
+{
+    private double overdraftLimit = 5000; 
+    public CurrentAccount(String accountHolder, double balance)
+    {
+        super(accountHolder, balance);
+    }
+
+    @Override
+    public void deposit(double amount)
+    {
+        if(amount>0)
+        {
+            balance = balance + amount;
+            System.out.println("₹" + amount +" deposited in Current Account...");
+        }
+        else 
+        {
+            System.out.println("Invalid Deposit Amount");
+        }
+    }
+    
+    @Override
+    public void withdraw(double amount)
+    {
+        if(amount > 0 && (balance + overdraftLimit) >= amount) //that amount you want to withdraw is less or equal to the present balance amount in your bank 
+        {
+            balance = balance - amount;
+            System.out.println("₹" + amount + " withdrawn from Current Account...");
+        }
+        else 
+        {
+            System.out.println("Withdrawl exceeds overdraft limit!");
+        }
+    }
+
+}
