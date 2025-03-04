abf698bab757f805f0c87f8aa47edcc834890f76
the-madhur <madhurhp245@gmail.com>
2025-03-04 11:07:03 +0530

    Abstract 2nd file commit

---
+++ src/Bank/SavingAccount.java
@@ -0,0 +1,15 @@
+package Bank;
+
+public class SavingAccount extends BankAccount 
+{
+
+    public void deposit()
+    {
+        System.out.println("Enter amount you want to deposit");
+    }
+
+    public void withdraw()
+    {
+        System.out.println("Enter amount you want to deposit");
+    }
+}
