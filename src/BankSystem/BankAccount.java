3e6a25905520c396e99586a54f90746191982224
the-madhur <madhurhp245@gmail.com>
2025-03-04 11:31:57 +0530

    bankCcc

---
+++ src/BankSystem/BankAccount.java
@@ -0,0 +1,21 @@
+package BankSystem;
+
+public abstract class BankAccount 
+{
+    protected String accountHolder;
+    protected double balance;
+
+    public BankAccount(String accountHolder, double balance)
+    {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+
+    public void checkBalance()
+    {
+        System.out.println(accountHolder + "'s current balance: ₹"+ balance);
+    }
+}
