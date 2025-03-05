33e18571b44c11ca3415e08a3dc5ec4003c8d8d5
the-madhur <madhurhp245@gmail.com>
2025-03-05 22:58:01 +0530

    Email FInal commit

--- src/Interface/EmailService.java
+++ src/Interface/EmailService.java
@@ -2,17 +2,23 @@ package Interface;
 
 public class EmailService implements NotificationsService
 {
+    private String email;
+    
+    public EmailService(String email)
+    {
+        this.email = email;
+    }
 
     @Override
     public void sendNotifiactions(String message) 
     {
-       System.out.println();
+       System.out.println("Sending email notifications to "+email);
     }
 
     @Override
     public void subscribeToTopic(String topic) 
     {
-        
+        System.out.println("Subscribed "+email+" email notifications for topic: ");
     }
 
 }
