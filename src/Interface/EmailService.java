2558214bd1a8d92f09e252431552ac2c5e296c1e
the-madhur <madhurhp245@gmail.com>
2025-03-05 16:21:22 +0530

    Email Commit Interface2

---
+++ src/Interface/EmailService.java
@@ -0,0 +1,18 @@
+package Interface;
+
+public class EmailService implements NotificationsService
+{
+
+    @Override
+    public void sendNotifiactions(String message) 
+    {
+       System.out.println();
+    }
+
+    @Override
+    public void subscribeToTopic(String topic) 
+    {
+        
+    }
+
+}
