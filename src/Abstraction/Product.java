44c8ccc7696248b2f8e50c37ba20fc78bf44966f
the-madhur <madhurhp245@gmail.com>
2025-03-05 13:54:33 +0530

    Product commit

---
+++ src/Abstraction/Product.java
@@ -0,0 +1,18 @@
+package Abstraction;
+
+public abstract class Product 
+{
+    protected String name;
+    protected double price;
+
+    public Product(String name, double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    abstract double calculateDiscount();
+    abstract double calculateTax();
+    abstract double calculateShippingCost();
+    
+}
