d8e6adb051cfc52341a1f6fde90339c99c329285
the-madhur <madhurhp245@gmail.com>
2025-03-06 09:50:50 +0530

    Audio commit

---
+++ src/Interface/AudioPlayer/AudioPlayerTest.java
@@ -0,0 +1,20 @@
+package Interface.AudioPlayer;
+
+public class AudioPlayerTest 
+{
+    public static void main(String[] args) 
+    {
+        AudioPlayer mp3 = new MP3Player();
+        mp3.play();
+        mp3.pause();
+        mp3.stop();
+        
+        System.out.println("----------------------------");
+
+        AudioPlayer wav = new WAVPlayer();
+        wav.play();
+        wav.pause();
+        wav.stop();
+    }
+
+}
