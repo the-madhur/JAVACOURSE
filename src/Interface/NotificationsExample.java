114fadf8367f5ad6a02e502ef2a2516c1631a5da
the-madhur <madhurhp245@gmail.com>
2025-03-20 15:35:27 +0530

    changes

--- src/Interface/NotificationsExample.java
+++ src/Interface/NotificationsExample.java
@@ -1,5 +1,6 @@
 package Interface;
 
+// Main Class
 public class NotificationsExample 
 {
     public static void main(String[] args) 
@@ -7,8 +8,8 @@ public class NotificationsExample
         NotificationsService emailService = new EmailService("usermadhur@gmail.com");
         NotificationsService smsService = new SMSnotificationService("+91729479272");
 
-        emailService.sendNotifiactions("Hello, this is an email notification");
-        smsService.sendNotifiactions("Hello, this is an SMS notification");
+        emailService.sendNotifications("Hello, this is an email notification");
+        smsService.sendNotifications("Hello, this is an SMS notification");
 
         emailService.subscribeToTopic("News");
         smsService.subscribeToTopic("Alerts");
